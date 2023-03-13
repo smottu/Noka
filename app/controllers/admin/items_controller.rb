b6ca3aacf912873da55d1b88039539b778be7ba4
smottu <106876134+smottu@users.noreply.github.com>
2023-03-13 23:49:56 +0000

    adminitem

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,35 +1,40 @@
 class Admin::ItemsController < ApplicationController
-  
+
   def new
     @item = Item.new
   end
-  
+
   def create
-    @item = Item.new
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path
+    else
+      render :new
+    end
   end
-  
+
   def index
     @items = Item.all
   end
-  
+
   def show
-    @item = Item.find(params_id)
+    @item = Item.find(params[:id])
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
-  
+
+
   private
-  
+
   def item_params
-    require.(:items).parmit(:item_name,:item_text,:item_amount,:item_image,:item_price,:is_active)
+    params.require(:item).parmit(:item_name)
   end
-  
+
 end
